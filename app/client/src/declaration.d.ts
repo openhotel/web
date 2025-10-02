4f57e8428b3490bf99ba31e8200029ef4517181e
pablo <pagoru@gmail.com>
2025-10-02 11:53:26 +0200

    bump: components library to v0.3.3 - fix #14

---
+++ app/client/src/declaration.d.ts
@@ -0,0 +1,9 @@
+// declaration.d.ts or styles.d.ts
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
